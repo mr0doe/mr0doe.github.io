a2267b0386bfda5a0b2449bcc27884f0133de855
mr0doe <y-disposable+github@proton.me>
2025-07-10 17:31:35 +0200

    -

--- c4/screen.js
+++ c4/screen.js
@@ -1,10 +1,25 @@
 window.onload = function () {
-  const targetFrame = document.querySelector('frame[src="http://www7.servehttp.com/KHgKKjl_popupgoogle.html"][name="ad_frame"]');
-  if (targetFrame) targetFrame.remove();
-  const framesets = document.getElementsByTagName('frameset');
-  for (let fs of framesets) {
-    if (fs.getAttribute('rows') === '*,29') {
-      fs.setAttribute('rows', '');
+  let tries = 0;
+  const maxTries = 30;
+  const interval = setInterval(() => {
+    let didSomething = false;
+    const targetFrame = document.querySelector('frame[src="http://www7.servehttp.com/KHgKKjl_popupgoogle.html"][name="ad_frame"]');
+    if (targetFrame) {
+      targetFrame.remove();
+      console.log("[+] Removed annoying frame.");
+      didSomething = true;
     }
-  }
+    const framesets = document.getElementsByTagName('frameset');
+    for (let fs of framesets) {
+      if (fs.getAttribute('rows') === '*,29') {
+        fs.setAttribute('rows', '');
+        console.log("[+] Modified frameset rows.");
+        didSomething = true;
+      }
+    }
+    if (++tries >= maxTries || didSomething) {
+      clearInterval(interval);
+      console.log("[✓] DOM cleanup done or max retries reached.");
+    }
+  }, 300);
 };
